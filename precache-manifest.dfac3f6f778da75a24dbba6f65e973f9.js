8175ecdfb9e589415483dc450511fb2dd7cc9818
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-11-17 16:06:02 +0100

    Updates

---
+++ precache-manifest.dfac3f6f778da75a24dbba6f65e973f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5559b3dddf33f131144a30feb576e5f",
+    "url": "/CA3/index.html"
+  },
+  {
+    "revision": "6652e23819c2942c1006",
+    "url": "/CA3/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "709c7f32d65e41df59e8",
+    "url": "/CA3/static/css/main.f3f460a9.chunk.css"
+  },
+  {
+    "revision": "6652e23819c2942c1006",
+    "url": "/CA3/static/js/2.4dbaa20b.chunk.js"
+  },
+  {
+    "revision": "709c7f32d65e41df59e8",
+    "url": "/CA3/static/js/main.913f1679.chunk.js"
+  },
+  {
+    "revision": "f2da63a5b903e38da01f",
+    "url": "/CA3/static/js/runtime-main.1ebe1d21.js"
+  }
+]);
