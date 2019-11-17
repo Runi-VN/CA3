5246959e105887bc96b6c3a76890afbe3032bbd6
Rúni VN <Runiniclasen@hotmail.com>
2019-11-17 22:11:57 +0100

    change Welcome component

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,81 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../Welcome.css";
+import React from 'react';
+import {Link} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../Welcome.css';
 
 const Welcome = () => {
-  return (
-    <>
-      <h1>Welcome Page</h1>
-      <h6>
-        <ins>After</ins> having set up the backend:
-      </h6>
-      <ol>
-        <li>
-          <code>npm install</code>
-        </li>
-        <li>
-          <code>npm start</code>
-        </li>
-      </ol>
-      <h6>Common issues:</h6>
-      <ul>
-        <li>
-          <code>npm audit fix --force</code>
-        </li>
-      </ul>
-      <h6>File overview</h6>
-      <label>
-        <i>src/components</i>
-      </label>
-      <br />
-      <ul className="border border-secondary customBorder">
-        <li>
-          <code>Welcome.jsx</code> - Where you are
-        </li>
-        <li>
-          <code>Swapi.jsx</code> - Handles fetch call to the backend (Using
-          <a target="_blank" rel="noopener noreferrer" href="https://swapi.co/">
-            {" "}
-            https://swapi.co/
-          </a>
-          )
-        </li>
-        <li>
-          <code>Login.jsx</code> - Handles login
-        </li>
-      </ul>
-      <br />
-      <label>
-        <i>src/</i>
-      </label>
-      <br />
-      <ul className="border border-secondary customBorder">
-        <li>
-          <code>apiFacade.jsx</code> - Utility to help with fetching,
-          login/logout
-        </li>
-        <li>
-          <code>App.js</code> - Main component, handles routes &{" "}
-          <code>loggedIn</code>
-        </li>
-        <li>
-          <code>index.js</code> - Render component
-        </li>
-        <li>
-          <code>settings.js</code> - Holds global settings, such as fetch URLs
-        </li>
+	return (
+		<>
+			<h1>How I utilized the start code</h1>
 
-        <span>
-          <i>CSS files</i>
-        </span>
-      </ul>
-      <p>
-        <Link to="#/login">Login</Link> using the credentials you set in the
-        backend <code>utils\SetupTestUsers.java</code>
-      </p>
-    </>
-  );
+			<ol>
+				<li>Forked, cloned the main repository</li>
+				<li>
+					Followed the frontend guide:{' '}
+					<a href="https://github.com/Runi-VN/CA3/blob/master/frontend/README.md">
+						https://github.com/Runi-VN/CA3/blob/master/frontend/README.md
+					</a>
+				</li>
+				<li>
+					Deployed using surge HTTPS:{' '}
+					<a href="	https://surge.sh/help/using-https-by-default">
+						https://surge.sh/help/using-https-by-default
+					</a>
+				</li>
+				<li>
+					Found an error:{' '}
+					<a href="https://github.com/facebook/create-react-app/issues/3294#issuecomment-339835457">
+						https://github.com/facebook/create-react-app/issues/3294#issuecomment-339835457
+					</a>
+					<ul>
+						<li>
+							Github Pages use <code>homepage</code> in{' '}
+							<code>package.json</code>
+						</li>
+						<li>
+							Surge is not very fond of this and tries erroneously to refer to{' '}
+							<code>homepage</code>
+						</li>
+					</ul>
+				</li>
+				<li>Actually deployed</li>
+			</ol>
+
+			<p>
+				<Link to="#/login">Login</Link> using the credentials you set in the
+				backend <code>utils\SetupTestUsers.java</code>
+			</p>
+		</>
+	);
 };
 
 export default Welcome;
